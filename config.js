8bfa2eb66b2b158e85549f0cf1d544db1aca087b
tawhidullahazimi-cyber <tawhidullahazimi@gmail.com>
2026-08-03 06:09:42 +0430

    Update owner number and session ID in config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['9876543210'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerNumber: ['93798649722'], // Add your number without + or spaces (e.g., 919876543210)
     ownerName: ['Knight Bot'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
     botName: 'Knight Bot Mini',
     prefix: ',',
     sessionName: 'session',
-    sessionID: process.env.SESSION_ID || '',
+    sessionID: process.env.SESSION_ID || 'KnightBot!H4sIAAAAAAAAA5VU25KiSBT8l3rFmFZAESM6Yrko0lzECyJu7EM1FFDcLQoRJ/z3Dbqnp+dhd7b3rSgq8uTJzHO+g7LCDTJQDxbfQU3wFVI0HGlfI7AAchtFiIARCCGFYAEMCW32uc+XW/GUGPKTXb3mzGZ+XR9f2nO5k1KDGmaNBcuNn8FjBOr2NcfBbwDZU6d3HOGK8dFiuTJZVVRtomgf69zdiw0nrJ3j0lD4vrCewWNAhJjgMl7WCSoQgbmBegdi8jX6c3278sT2UGdVRiLXd7NQbG6pUuDCaJi0K4/TyW01SVa59DX67p1GMpEm9TTYF1mioXmc28xxHM3JoZGZsnktJq61VbRD9k6/wXGJQj1EJcW0/7LulsSYMe8I2rgynDSw7PGraYbCQRT3e/sQxTynCqreFcLS/xrxKha2RZwwF0a9l5s4bBThfhcS5qqpd7JbWnF1u3g5f9Dz7lfiDvnISvZ/dIf6ybh1UBYr/7pvzrLjpBk5FHvsvVhqAKv2dJI8IQgE1v0afb5TVsfQ2lXdkjMyfb8Xjyvff1LlpZ2Nz/HcP29ac60aN3f8SR/SlvyOZUfNMBGe7P099hnIipImThS5Ym5sX/BVlRgnXsrONgd7tuiV1nXNYsoxx2ZyWO05WxfqyyneyY3j3wXBOtzHyj1kcPf81lGGej0Ei8ljBAiKcUMJpLgq3+7EEYDhdY8CguibumDuLPlps+7JBV7SfCXOZYNqbH30zEur0BMTmeeOuPZL6knPYARqUgWoaVC4xg2tSG+hpoExasDiz79GoEQ3+u7bUI2bjECESUPdsq3zCoYfpn78hEFQtSXd92WgDAdEwGL8eY0oxWXcDDK2JSRBgq9ISSBtwCKCeYN+NogICsGCkhb9HFqlCgfdPV7TzPNOBSNQvPmBQ7AAIieI8xkvCiy7mPB/NN+6ARXW9bcSUTAC+fsrfsbz07nAspOxOB8eDvePn/wGuBBRiPMGLICyWV+e+E5bGofgKlqaJlmxpMQS+OznIxfvwtvBtbfjhN3UQmGuScpesJkq8vkUxtYW8ooa+qqJD1sr5Z//AWQIlaJPYH6uVTyfLV+fNMfnd9AzUu++m4YzUStuMNN3OvR5fL9qZ53NIlG3eaHYXnceIc5mm0aOakZVIrvj7TlR3LEkDyEagRBdcYB+LXbPTJu/0R0XVJdL5HB6E83IE2pRS/KNnm64aDdpy12oxUIhrZfZzF6/pKXueOLMY2BpFoXbT2lGU2O72a6Sa/GyXaY/Evs2MfmPTYXfwjQ4NXxGGL0NfgkH//7TuXfeQ77Gj9EvED82yb9Mo3w4mfPU18P0gqb4NuPFS4X5LvaOnK8utVaKOU/q+Wys6gx4PP4agTqHNKpIMSycMiQVDsEIkKodAquXUfWbYoqU6eo2Xg2N57Ch0ucQHHCBGgqLGiwmwnwqsGNuxj7+BgSwQ4U5BwAA',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
